44256694309527a97d79728b0c32d36a142b34c6
Aaron Joel Lontsi <1327793+aaronjoel@users.noreply.github.com>
2025-05-01 13:23:49 +0200

    Added a math module to experiment with import and export.

---
+++ math.js
@@ -0,0 +1,12 @@
+// Within a file called math.js, we export the following functions:
+
+// Named export
+export function add(num1, num2) {
+    return num1 + num2;
+  }
+  
+  // Default export
+  export default function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  
